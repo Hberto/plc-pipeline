b9ecbb9ee31887a4921e0cb846990f30e2b5b666
Herberto <wernerherberto@gmail.com>
2022-04-14 15:04:29 +0200

    rename class and test build moved to new package

---
+++ plc4x_connector/src/main/java/ApplicationStarter.java
@@ -0,0 +1,15 @@
+
+//ToDo: comments
+//ToDo: start the application here
+//ToDo: connector to kafka connect
+
+/**
+ * This class is the main entry of the plc4x connector application
+ * with the Programmable Logic Controller (PLC).
+ * ToDo: Update description
+ * @author Herberto Werner
+ */
+
+public class ApplicationStarter {
+    //ToDo: final pipeline impl starter
+}
