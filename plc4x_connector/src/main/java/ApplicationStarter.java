d38baa2f4191d1a742a602976d9fa81253aab070
Herberto <wernerherberto@gmail.com>
2022-06-07 22:35:14 +0200

    first version of app starter

--- plc4x_connector/src/main/java/ApplicationStarter.java
+++ plc4x_connector/src/main/java/ApplicationStarter.java
@@ -3,7 +3,11 @@
 //ToDo: start the application here
 //ToDo: connector to kafka connect
 
+import KafkaPLCProducer.AvroProducer;
 import KafkaPLCProducer.producer.PLCData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import testPipeline.Simple_Pipeline;
 
 /**
  * This class is the main entry of the plc4x connector application
@@ -12,17 +16,34 @@ import KafkaPLCProducer.producer.PLCData;
  * @author Herberto Werner
  */
 
+//ToDo: Optional: Über Konsole: Einstellbar welche Connection zu PLC usw.
+
 public class ApplicationStarter {
-    //ToDo: final pipeline impl starter
-    PLCData plcdata = PLCData.newBuilder()
-            .setData(345345)
-            .setSenderName("teste")
-            .setDataType("Long")
-            .setOrderNr(12001)
-            .setOperation("Read")
-            .build();
+
+    private static final Logger log = LoggerFactory.getLogger(Simple_Pipeline.class);
+    private static final int TRYOUTS = 10;
+    //ToDo: Integrate PLC4X
 
 
-    //ToDo: Optional: Über Konsole: Einstellbar welche Connection zu PLC usw.
+    public static void main(String[] args) {
+
+        PLCData plcdata = PLCData.newBuilder()
+                .setData(345345)
+                .setSenderName("teste")
+                .setDataType("Long")
+                .setOrderNr(12001)
+                .setOperation("Read")
+                .build();
+
+        log.info("Starting Application Starter.......");
+        AvroProducer avroProducer = new AvroProducer();
+        log.info("Sending Message.......");
+        for(int i = 0; i < TRYOUTS; i++) {
+            avroProducer.runProducer(plcdata);
+            log.info("Message SENT.......");
+        }
+
+    }
+
 
 }
