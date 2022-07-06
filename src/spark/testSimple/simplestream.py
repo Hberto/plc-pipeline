8be756a311a1f6c37e40ca2950efca86b3a7ba2c
Hberto <wernerherberto@gmail.com>
2022-07-06 21:05:15 +0200

    connection to spark master and worker and kafka works

--- src/spark/testSimple/simplestream.py
+++ src/spark/testSimple/simplestream.py
@@ -1,15 +1,10 @@
-import json
-
-import pandas as pd
-import requests
 from pyspark.sql import SparkSession
-from pyspark import SparkFiles
 
 appName = "Testing the Stream with Kafka"
 topic = "plcDataTest2"
 bootstrap_server = "bla:9092"
 path = "C:/Users/Herberto/Desktop/UNI/Bachelor-Arbeit/plc-pipeline/docker_containers/spark/teste.txt"
-csv_url = "https://raw.githubusercontent.com/lrjoshi/webpage/master/public/post/c159s.csv"
+
 
 def update_spark_log_level(spark):
     spark.sparkContext.setLogLevel('info')
@@ -23,16 +18,51 @@ def stream_testing():
     spark: SparkSession = SparkSession \
         .builder \
         .master("spark://127.0.0.1:7077") \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
         .appName(appName) \
         .getOrCreate()
 
-
+    # spark.sparkContext._jsc.addJar("/home/herberto/Schreibtisch/BA/spark-sql-kafka-0-10_2.13-3.3.0.jar")
     # Alternative 2
-    # sc = spark.sparkContext
-    # sc.setLogLevel(sc, 'info')
     logger = update_spark_log_level(spark)
-    logger.info("what")
+    # sc = spark.sparkContext
+    logger.info("++++++Starting Job++++++")
 
+    logger.info("++++++Reading Stream from Kafka++++++")
+    df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:29092") \
+        .option("subscribe", "plcDataString") \
+        .load()
+
+    logger.info("++++++Printing from Kafka++++++")
+    query = df.selectExpr("CAST(value AS STRING)")
+    print(query)
+
+    #query = df.selectExpr("CAST(value AS STRING)") \
+    #    .writeStream \
+    #    .format("console") \
+    #    .start()
+    #query.awaitTermination()
+    # ToDo: Check Avro Schema
+    # Should be later, check the connection to kafka first
+
+    # ToDo: create Dataframe and reading Data from Kafka
+    # https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html#deploying
+    # ...express streaming aggregations, event-time windows, stream-to-batch joins
+
+    # ToDo: Monitor Streaming queries -
+    #  https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html#monitoring-streaming-queries
+
+    # ToDo: Write StreamingQuery to datenbank
+
+    ###### Useful Tips ######
+    # Note, you can identify whether a DataFrame/Dataset has streaming data or not by using
+    # df.isStreaming
+
+
+def run_spark_job_example(spark) -> None:
     from pyspark.sql.types import IntegerType
     from pyspark.sql.types import StructField
     from pyspark.sql.types import StructType
@@ -62,21 +92,3 @@ def stream_testing():
     )
 
     second_highest_salary.show()
-
-    # ToDo: Check Avro Schema
-    # Should be later, check the connection to kafka first
-
-    # ToDo: create Dataframe and reading Data from Kafka
-    # https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html#deploying
-    # ...express streaming aggregations, event-time windows, stream-to-batch joins
-
-    # ToDo: Do pyspark streams with spark video
-
-    # ToDo: Monitor Streaming queries -
-    #  https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html#monitoring-streaming-queries
-
-    # ToDo: Write StreamingQuery to datenbank
-
-    ###### Useful Tips ######
-    # Note, you can identify whether a DataFrame/Dataset has streaming data or not by using
-    # df.isStreaming
