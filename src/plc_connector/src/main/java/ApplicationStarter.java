d2965de27d3de7108efbc3d082c7e0f23531b117
Herberto <wernerherberto@gmail.com>
2022-10-24 12:17:10 +0000

    to application starter for plc4x connector updated and comments deleted

--- src/plc_connector/src/main/java/ApplicationStarter.java
+++ src/plc_connector/src/main/java/ApplicationStarter.java
@@ -1,12 +1,7 @@
 
-//ToDo: comments
-//ToDo: start the application here
-//ToDo: connector to kafka connect
-
-import KafkaPLCProducer.Avro.AvroProducer;
-import KafkaPLCProducer.JSON.JSONProducer;
-import KafkaPLCProducer.StringFormat.StringProducer;
-import KafkaPLCProducer.producerData.PLCData;
+import PLC4X_Connector.ReadOperations.ReadOperation;
+import PLC4X_Connector.connection.ConnectionHandler;
+import org.apache.plc4x.java.PlcDriverManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,40 +9,30 @@ import org.slf4j.LoggerFactory;
 /**
  * This class is the main entry of the plc4x connector application
  * with the Programmable Logic Controller (PLC).
- * ToDo: Update description
  * @author Herberto Werner
  */
 
-//ToDo: Optional: Über Konsole: Einstellbar welche Connection zu PLC usw.
 
 public class ApplicationStarter {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationStarter.class);
-    private static final int TRYOUTS = 10;
+    private static final String CON_ADDR = "s7://192.168.0.2";
+
+    private static PlcDriverManager driverManager = null;
+    private static ConnectionHandler conHandler = null;
+    private static ReadOperation reader = null;
 
 
     public static void main(String[] args) {
 
-        PLCData plcdata = PLCData.newBuilder()
-                .setData(345345L)
-                .setSenderName("teste")
-                .setDataType("Long")
-                .setOrderNr(12001L)
-                .setOperation("Read")
-                .build();
+        driverManager = new PlcDriverManager();
+        conHandler = new ConnectionHandler(CON_ADDR, driverManager);
 
-
-        log.info("Starting Application Starter.......");
-        AvroProducer avroProducer = new AvroProducer();
-        JSONProducer jsonProducer = new JSONProducer();
-        StringProducer stringProducer = new StringProducer();
-        log.info("Sending Message.......");
-        //avroProducer.runProducer(plcdata);
-        //avroProducer.runProducerSimple("Hey it worked");
-        //jsonProducer.runProducer();
-        stringProducer.runProducerString();
-        log.info("+++++++++++++++++DONE++++++++++++++++");
-
+        //Returns a PLCConnection with a driverManager or driverManagerPool
+        reader = new ReadOperation(conHandler.connect(true));
+        log.info("Test: Connection success");
+        reader.readSingleField("test2","%I0.1:BOOL");
+        log.info("Read success");
     }
 
 
