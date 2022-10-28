90a118439cada303f16900ed55cd590e26a8f32c
HBerto <herberto.werner@haw-hamburg.de>
2022-10-28 14:20:44 +0200

    column name and type changed

--- docker_containers/cassandraExecutor/plc_data_cassandra.sh
+++ docker_containers/cassandraExecutor/plc_data_cassandra.sh
@@ -7,6 +7,6 @@ done
 
 echo "Creating keyspace and table..."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (sensor_id int, temperature int, PRIMARY KEY ((sensor_id), temperature));"
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (value int, timestamp timestamp, PRIMARY KEY ((value), timestamp));"
 
 echo "Done Creating keyspace and table..."
