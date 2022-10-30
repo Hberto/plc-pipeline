966236935221c4184a1339897c5456a5c14a7daf
HBerto <herberto.werner@haw-hamburg.de>
2022-10-30 20:05:33 +0100

    sh script changing table definition by adding topic

--- docker_containers/cassandraExecutor/plc_data_cassandra.sh
+++ docker_containers/cassandraExecutor/plc_data_cassandra.sh
@@ -7,6 +7,6 @@ done
 
 echo "Creating keyspace and table..."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (value int, timestamp timestamp, PRIMARY KEY ((value), timestamp));"
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (topic text, value int, timestamp timestamp, PRIMARY KEY ((topic), timestamp));"
 
 echo "Done Creating keyspace and table..."
--- docker_containers/plc_data_cassandra.sh
+++ docker_containers/plc_data_cassandra.sh
@@ -2,6 +2,6 @@
 
 echo "Creating keyspace and table..."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
-cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (value int, timestamp timestamp, PRIMARY KEY ((value), timestamp));"
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (topic text, value int, timestamp timestamp, PRIMARY KEY ((topic), timestamp));"
 
 echo "Done Creating keyspace and table..."
