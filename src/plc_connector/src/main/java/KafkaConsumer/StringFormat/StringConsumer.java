aacb37aa3d7b4ec52c25c599184b80e1726b7d9b
Herberto <wernerherberto@gmail.com>
2022-11-04 10:40:08 +0100

    Add Comments

--- src/plc_connector/src/main/java/KafkaConsumer/StringFormat/StringConsumer.java
+++ src/plc_connector/src/main/java/KafkaConsumer/StringFormat/StringConsumer.java
@@ -12,8 +12,13 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 import java.util.Collections;
-import java.time.Duration;  
+import java.time.Duration;
 
+/**
+ * A simple Kafka Consumer Class with static parameters.
+ * Disclaimer: Only use for latency measurement.
+ * @author Herberto Werner
+ */
 public class StringConsumer {
 
     //Configs for Kafka
@@ -21,21 +26,30 @@ public class StringConsumer {
     private final static String GROUP_ID = "plcpipeline";
     private final static String TOPIC = "12003800_test2";
     private static final Logger log = LoggerFactory.getLogger(StringConsumer.class);
-
     private Consumer <String, String> consumer = null;
 
+    /**
+     * Constructor of the Consumer class.
+     */
     public StringConsumer() {
         consumer = createConsumer();
     }
 
+    /**
+     * Runs a Consumer with static configs. Prints out basic record metadata.
+     */
     public void runConsumer() {
         ConsumerRecords<String,String> records=consumer.poll(Duration.ofMillis(100));
         for(ConsumerRecord<String, String> record : records) {
-            System.out.println("+++CONSUMER: "+ "Topic: " + record.topic() + "Timestamp in ms: " + record.timestamp());
+            log.info("+++Reading Consumption!+++");
+            System.out.println("CONSUMER: "+ "Topic: " + record.topic() + "Timestamp in ms: " + record.timestamp());
         } 
     }
 
-
+    /**
+     * Creates a Consumer with static Properties and returns it.
+     * @return a Consumer with configs
+     */
      private static Consumer<String, String> createConsumer() {
       final Properties props = new Properties();
       props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
@@ -46,7 +60,7 @@ public class StringConsumer {
       // Create the consumer using props
       Consumer<String, String> consumer = new KafkaConsumer<>(props);
 
-      // Subscribe to the topic.
+      // Subscribe to the topic
       consumer.subscribe(Collections.singletonList(TOPIC));
       return consumer;
      }
