fdb775709df8f80a57a193faa0ee156fb32895e2
Hberto <wernerherberto@gmail.com>
2022-11-15 23:23:19 +0100

    new table for cassandra for evaluation

--- docker_containers/cassandraExecutor/plc_data_cassandra.sh
+++ docker_containers/cassandraExecutor/plc_data_cassandra.sh
@@ -8,5 +8,5 @@ done
 echo "Creating keyspace and table..."
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE KEYSPACE IF NOT EXISTS test WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};"
 cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.test (topic text, value int, timestamp timestamp, PRIMARY KEY ((topic), timestamp));"
-
+cqlsh cassandra -u cassandra -p cassandra -e "CREATE TABLE IF NOT EXISTS test.kafka (topic text, value int, current_timestamp timestamp, PRIMARY KEY ((topic), current_timestamp));"
 echo "Done Creating keyspace and table..."
