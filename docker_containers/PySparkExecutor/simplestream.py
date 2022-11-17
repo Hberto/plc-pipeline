f56ae57a8e13d514a9a0b670eb14f27fa05f26c5
Hberto <wernerherberto@gmail.com>
2022-11-18 00:54:29 +0100

    measurement metrics deleted and kafkaWriteTable deleted

--- docker_containers/PySparkExecutor/simplestream.py
+++ docker_containers/PySparkExecutor/simplestream.py
@@ -8,40 +8,6 @@ import pandas as pd
 # Kafka Parameters and Configs
 appNameKafka = "Testing the Stream with Kafka"
 
-# Measurement Parameters
-
-# Spark Measurement Variables
-header_spark = ['SparkProcessingTimeMeasureNr', 'TSDateformat', 'ClockTime1', 'ClockTime2', 'ProcessingTimeLong']
-my_file_spark = '/scripts/sparkProcess.csv'
-
-
-
-# Measurement functions
-def create_timestamp_with_header_csv(my_file, header):
-
-    print("Creating csv file with headers")
-    with open(my_file, 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-
-def write_row(data, my_file):
-    print("Add Data")
-    with open(my_file, 'a', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(data)
-
-def delete_all_rows(my_file):
-    print("Delete all rows")
-    data = pd.read_csv(my_file)
-    data = data[0:0]
-    data.to_csv(my_file, index=False)
-
-def delete_row(row, my_file):
-    print("Delete row")
-    data = pd.read_csv(my_file)
-    data.drop(row, inplace=True, axis=0)
-    data.to_csv(my_file, index=False)
-
 # Updating Log Level
 def update_spark_log_level(spark):
     spark.sparkContext.setLogLevel('error')
@@ -116,14 +82,16 @@ def stream_testing():
         .option("kafka.bootstrap.servers", "kafka:9092") \
         .option("topic", "12003800_test2") \
         .option("checkpointLocation", "/tmp") \
-        .start()
+        .start() \
+        .withColumn("current_timestamp", F.current_timestamp())
+
     
     print("++++++Select and Processing Section++++++")
     # Measurment of KafkaQueueTS
     df_kafka_ts = df.selectExpr("CAST(topic as STRING)","CAST(value AS STRING)","CAST(current_timestamp AS Timestamp)")
 
     # Filter Data for Cassandra
-    df = df.selectExpr("CAST(topic as STRING)","CAST(value AS STRING)", "CAST(timestamp AS Timestamp)")
+    df = df.selectExpr("CAST(topic as STRING)","CAST(value AS STRING)", "CAST(timestamp AS Timestamp)", "CAST(current_timestamp AS Timestamp)")
     # Change Column Type
     df_new = df.withColumn("value", df["value"].cast(IntegerType()))
 
@@ -136,11 +104,11 @@ def stream_testing():
         .start()
 
     # Comment when measuring Spark jobs duration 
-    kafka_ts_query = df_kafka_ts.writeStream \
-        .trigger(processingTime="0 seconds") \
-        .outputMode("append") \
-        .foreachBatch(saveKafkaTSToCassandra) \
-        .start()
+    #kafka_ts_query = df_kafka_ts.writeStream \
+    #    .trigger(processingTime="0 seconds") \
+    #    .outputMode("append") \
+    #    .foreachBatch(saveKafkaTSToCassandra) \
+    #    .start()
     
     print("++Running Query Stream..waiting for Termination++")
     query.awaitTermination()
