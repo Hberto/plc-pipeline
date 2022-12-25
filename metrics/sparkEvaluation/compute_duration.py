ca496c55fb3f924d4bd823e6bba5637f1c0a05a3
Hberto <wernerherberto@gmail.com>
2022-12-25 11:29:51 +0100

    spark eval scripts and files

--- metrics/sparkEvaluation/compute_duration.py
+++ metrics/sparkEvaluation/compute_duration.py
@@ -1,7 +1,7 @@
 import re
 import math
 
-path = 'data.txt'
+path = './BEST/data_10000.txt'
 
 pattern = r'"batchDuration":(\d+),'
 with open(path, 'r') as f:
@@ -10,4 +10,7 @@ matches = re.findall(pattern, text)
 
 conv = [int(match) for match in matches][2:]
 mean = sum(conv) / (len(conv))
-print(mean)
+
+print("Average Time of Batchduration ms = ",  mean)
+print("Minimum Time of Batchduration in ms = ", min(conv))
+print("Maximum Time of Batchduration in ms = ", max(conv))
